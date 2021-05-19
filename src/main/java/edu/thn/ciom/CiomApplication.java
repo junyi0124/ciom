dbdbae864a419746e68a4a97cfa745786fa2e13f
junyi <junyi0124@msn.com>
2021-05-19 11:49:39 +0800

    init

---
+++ src/main/java/edu/thn/ciom/CiomApplication.java
@@ -0,0 +1,13 @@
+package edu.thn.ciom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiomApplication.class, args);
+	}
+
+}
