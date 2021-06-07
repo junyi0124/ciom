221454eda0b83b1b86a5c38d65be1812df355ae5
junyi <junyi0124@msn.com>
2021-06-07 15:07:03 +0800

    add controller for file handling

--- src/main/java/edu/thn/ciom/controller/FileController.java
+++ src/main/java/edu/thn/ciom/controller/FileController.java
@@ -1,5 +1,7 @@
 package edu.thn.ciom.controller;
 
+import edu.thn.ciom.service.FileService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -10,13 +12,18 @@ import javax.servlet.http.HttpServletResponse;
 @RequestMapping("/file")
 public class FileController {
 
+    @Autowired
+    private FileService fileService;
+
+
     @PostMapping("/{id}")
     public void upload(@RequestParam("file")MultipartFile file) {
+        fileService.upload(file);
 
     }
 
     @GetMapping("/{id}/{name}")
     public void download(@PathVariable String id, @PathVariable String name, HttpServletResponse response) {
-
+        fileService.getFile(file);
     }
 }
